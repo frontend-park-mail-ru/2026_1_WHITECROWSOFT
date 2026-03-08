596442f417dc251bfae36bd1734bd763e119664b
Mr-None-rM <matvej.r_2@inbox.ru>
2026-03-08 15:44:08 +0300

    Добавил 404 страницу

---
+++ src/pages/not-found/notFoundPage.js
@@ -0,0 +1,12 @@
+export function initNotFoundPage() {
+	const app = document.querySelector('#app');
+	if (app) {
+		app.innerHTML = `
+            <div style="text-align:center;padding:50px">
+                <h1>404</h1>
+                <p>Страница не найдена</p>
+                <a href="/signin">Вернуться на вход</a>
+            </div>
+        `;
+	}
+}
