fe3fc788cbfeb2dfb76af947a54e47252e8bb19e
Mr-None-rM <matvej.r_2@inbox.ru>
2026-03-09 13:15:31 +0300

    Убрал избыточные функции в sidebar и добавил документацию

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,9 +1,22 @@
 import Handlebars from 'handlebars';
 import { router } from '../../route/router.js';
+import { render } from '../../utils/utils.js';
 import { authService } from '../../services/authService.js';
 import './sidebar.css';
 
+/**
+ * Класс дял боковой панели приложения
+ * @class SideBar
+ * @classdesc Управляет отображением и взаимодействием с боковой панелью,
+ * содержит список заметок, инструменты и кнопку выхода
+*/
 export class Sidebar {
+	/**
+	 * Создаёт экземпляр боковой панели
+	 * @param {string} containerSelector - ID элемнта контейнера
+	 * @param {Object} initialState - начальное состояние панели
+	 * @param {Function} onNoteClick - функция, вызываемая при нажатии на заметку 
+	*/
 	constructor(containerSelector, initialState = {}, onNoteClick) {
 		this.container = document.getElementById(containerSelector);
 		this.state = initialState;
@@ -11,19 +24,27 @@ export class Sidebar {
 		this.onNoteClick = onNoteClick;
 	}
 
+	/**
+	 * Инициализирует боковую панель
+	 * @async
+	 * @throws {Error} Если не удалось загрузить шаблон
+	*/
 	async init() {
 		const response = await fetch('/components/sidebar/sidebar.hbs');
+		if (!response.ok) {
+			throw new Error(`Failed to load template: ${response.status}`);
+		}
 		this.template = Handlebars.compile(await response.text());
-
-		this.render();
-	}
-
-	render() {
-		if (!this.template) return;
-		this.container.innerHTML = this.template(this.state);
+		render(this.container, this.template);
 		this.bindEvents();
 	}
 
+	/**
+	 * Навешивает обработчики событий на элементы панели
+	 * @private 
+	 * @listens click#logoutBtn - Клик по кнопке выхода
+	 * @listens click#[data-note-id] - Клик по элементу заметки 
+	*/
 	bindEvents() {
 		const logoutBtn = document.getElementById('logoutBtn');
 		if (logoutBtn) {
