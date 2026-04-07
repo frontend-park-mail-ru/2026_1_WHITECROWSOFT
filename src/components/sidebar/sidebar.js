f8c8c632ebf5a5b0ade7c6248b4b10789bbccfd4
Mr-None-rM <matvej.r_2@inbox.ru>
2026-04-07 20:14:16 +0300

    [fix] Изменил логику работы sidebar для взаимодействия с заметками

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,12 +1,14 @@
 import Handlebars from 'handlebars';
-import { router } from '../../route/router.js';
-import { authService } from '../../services/authService.js';
+import { bindNavigationEvents, updateActiveNote, startInlineEdit } from './sidebarEvents.js';
+import { NotePopup } from '../popups/notePopup/notePopup.js';
 import { render } from '../../utils/utils.js';
 import './sidebar.css';
 import templateText from './sidebar.hbs?raw';
+import { store } from '../../store.js';
+import { noteService } from '../../services/noteService.js';
 
 /**
- * Класс дял боковой панели приложения
+ * Класс для боковой панели приложения
  * @class SideBar
  * @classdesc Управляет отображением и взаимодействием с боковой панелью,
  * содержит список заметок, инструменты и кнопку выхода
@@ -18,11 +20,11 @@ export class Sidebar {
 	 * @param {Object} initialState - начальное состояние панели
 	 * @param {Function} onNoteClick - функция, вызываемая при нажатии на заметку
 	 */
-	constructor(containerSelector, initialState = {}, onNoteClick) {
+	constructor(containerSelector) {
 		this.container = document.getElementById(containerSelector);
-		this.state = initialState;
 		this.template = null;
-		this.onNoteClick = onNoteClick;
+		this.currentPopup = null;
+		this.state = store.getState();
 	}
 
 	/**
@@ -33,7 +35,7 @@ export class Sidebar {
 	async init() {
 		this.template = Handlebars.compile(templateText);
 		render(this.container, this.template(this.state));
-		this.bindEvents();
+		this._bindEvents();
 	}
 
 	/**
@@ -42,32 +44,123 @@ export class Sidebar {
 	 * @listens click#logoutBtn - Клик по кнопке выхода
 	 * @listens click#[data-note-id] - Клик по элементу заметки
 	 */
-	bindEvents() {
-		const logoutBtn = document.getElementById('logoutBtn');
-		if (logoutBtn) {
-			logoutBtn.addEventListener('click', async () => {
-				console.log('[MainPage] Logout clicked');
-				try {
-					await authService.logOut();
-					console.log('[MainPage] signOut() completed');
-					// router.clearSessionCache();
-					router.replace('/signin');
-				} catch (err) {
-					console.error('[MainPage] Logout error:', err);
-					// router.clearSessionCache();
-					router.replace('/signin');
+	_bindEvents() {
+		bindNavigationEvents(this.container, (noteId) => {
+			noteService.getNote(noteId)
+		});
+
+		const addBlockBtn = this.container.querySelector('#addBlockBtn');
+		if (addBlockBtn) {
+			addBlockBtn.addEventListener('click', async () => {
+				const activeNoteId = store.getActiveNoteId();
+				if (activeNoteId) {
+					await noteService.createBlock(activeNoteId, { type: 'text', content: '' });
 				}
 			});
 		}
 
-		this.container.addEventListener('click', (e) => {
-			const noteItem = e.target.closest('[data-note-id]');
-			if (noteItem) {
-				const noteId = noteItem.dataset.noteId;
-				if (this.onNoteClick) {
-					this.onNoteClick(noteId);
-				}
-			}
+		this.container.addEventListener('sidebar:openPopup', (e) => {
+            const { noteId, anchor } = e.detail;
+            this._openNotePopup(noteId, anchor);
+        });
+
+		this.container.addEventListener('sidebar:noteChanged', () => {
+            this.currentPopup?.close();
+        });
+
+		store.subscribe('notes', (notes) => {
+			this._updateNotes(notes);
+		});
+
+		store.subscribe('activeNoteId', (noteId) => {
+			this._setActiveNote(noteId);
+		});
+
+		store.subscribe('user', (user) => {
+			this._updateUser(user);
+		});
+
+		document.addEventListener('sidebar:startInlineEdit', (e) => {
+			const { noteId } = e.detail;
+			this._startInlineEdit(noteId);
 		});
 	}
+
+	_openNotePopup(noteId, anchorElement) {
+		this.currentPopup?.close();
+		this.currentPopup = new NotePopup(noteId, anchorElement);
+		this.currentPopup.open();
+
+		this.container.dispatchEvent(new CustomEvent('sidebar:popupOpened', {
+            detail: { 
+                noteId, 
+                popupElement: this.currentPopup.getElement() 
+            }
+        }));
+	}
+
+	_setActiveNote(noteId) {
+		this.state.activeNoteId = noteId;
+		updateActiveNote(this.container, noteId);
+		const addBlockBtn = this.container.querySelector('#addBlockBtn');
+		if (addBlockBtn) {
+			addBlockBtn.disabled = !noteId;
+		}
+		this.container.dispatchEvent(new CustomEvent('sidebar:noteChanged'))
+	}
+
+	_updateNotes(notes) {
+		this.state.notes = notes;
+		const notesList = this.container.querySelector('.notesList');
+		if (notesList) {
+			const notesTemplateString = `
+				{{#each notes}}
+					<div class="noteItem {{#if (eq this.ID ../activeNoteId)}}active{{/if}}" data-note-id="{{this.ID}}">
+						<img src="/icons/{{this.icon}}.svg" class="icon" />
+						<div class="noteItemTitle">{{this.title}}</div>
+						<div class="noteItemActions">
+							<button class="noteActionBtn addSubnoteBtn" data-action="addSubnote" data-note-id="{{this.ID}}">
+								<img src="/icons/add.svg" class="icon" />
+							</button>
+							<button class="noteActionBtn settingsBtn" data-action="settings" data-note-id="{{this.ID}}">
+								<img src="/icons/more.svg" class="icon" />
+							</button>
+						</div>
+					</div>
+				{{else}}
+					<div class="emptyNotes">
+						<p>У вас пока нет заметок</p>
+					</div>
+				{{/each}}
+			`;
+			const notesTemplate = Handlebars.compile(notesTemplateString);
+			notesList.innerHTML = notesTemplate(this.state);
+		}
+	}
+
+	_updateUser(user) {
+		this.state.user = user;
+		const usernameSpan = document.getElementById('profile-username');
+		if (usernameSpan && user) {
+			usernameSpan.textContent = user.username || 'Пользователь';
+		}
+	}
+
+	_startInlineEdit(noteId) {
+		const noteItem = this.container.querySelector(`.noteItem[data-note-id="${noteId}"]`);
+		if (noteItem) {
+			const noteItemTitle = noteItem.querySelector('.noteItemTitle');
+			if (noteItemTitle) {
+				startInlineEdit(noteItemTitle, noteId);
+			}
+		}
+	}
+
+	closePopup() {
+        this.currentPopup?.close();
+    }
+
+    destroy() {
+        this.currentPopup?.close();
+    }	
 }
