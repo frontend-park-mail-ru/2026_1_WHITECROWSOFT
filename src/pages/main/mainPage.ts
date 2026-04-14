4fafe5877b98ba33c9cb545ece9d2891a4019934
Andrey Nikitin <andrew@a71.su>
2026-04-14 14:56:02 +0300

    [fix] Capitalise B in imageBlock selector

--- src/pages/main/mainPage.ts
+++ src/pages/main/mainPage.ts
@@ -509,7 +509,7 @@ async function _updateBlocksInDOM(blocks: Block[]): Promise<void> {
 					);
 
 					blockEl = document.createElement('div');
-					blockEl.className = 'note__block note__imageblock';
+					blockEl.className = 'note__block note__imageBlock';
 					blockEl.dataset.blockId = String(block.id);
 
 					const img = document.createElement('img');
