0034086c3a908ca03811eeb9db6df203f24e6799
Andrey Nikitin <andrew@a71.su>
2026-05-28 10:20:35 +0300

    [style] Improve subnote block's SCSS

--- src/pages/main/mainPage.ts
+++ src/pages/main/mainPage.ts
@@ -43,7 +43,7 @@ export async function initMainPage(
 		}
 	}
 	const sharedNoteId = data?.query?.note;
-	let noteIdToOpen: string | number | undefined | null = null;
+	let noteIdToOpen: string | number | undefined | null;
 	if (sharedNoteId) {
 		noteIdToOpen = sharedNoteId;
 	} else {
